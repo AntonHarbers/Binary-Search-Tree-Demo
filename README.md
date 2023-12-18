d83fd9067a24da84897766b2c90ad0efa1ac4327
Anton Harbers <Anton.Harbers23@gmail.com>
2023-12-18 17:10:35 +0100

    Refactored code, reworked and made visualization functional, started documentation, font, favicon, folder structure

--- README.md
+++ README.md
@@ -4,10 +4,39 @@ Implemenation of a binary search tree for the odin project.
 
 Currently working on a visualization of the different functionality.
 
-Refactor Script and make it work
-Make App look nicer
-Add Favicon
-Add Google Fonts
-Animations
-Finish Visualization
-Add Documentation
+[Live Link]()
+
+![Repo Image](/images/repoImage.png)
+
+## Folder Structure
+
+```
+    /.git           -> This git repository
+    /bst            -> Contains BST and Tests
+        bst.js          -> Tree and Node Class, prettyPrint function
+        bst.test.js     -> All the teste for the BST
+        driver.js       -> Assignment for the Odin Project
+    /coverage       -> Util folder for JEST tests
+    /images         -> Favicon and Repo Image
+    /node_modules   -> Node Dependencies
+    /visualization
+        script.js       -> Logic for the visualization
+        style.css       -> Styling for the front end
+    .gitignore      -> Git Ignore for this repository
+    index.html      -> Entry point for the Visualization
+    package-lock.js -> NodeJS Util
+    package.json    -> NodeJS Util
+    README.md       -> This readme file
+```
+
+## Key Concepts
+
+### Binary Search Trees
+
+### Recursion and Stack Overflows
+
+### Testing with JEST
+
+### Visualization
+
+## Final Notes
--- visualization/script.js
+++ visualization/script.js
@@ -1,56 +1,44 @@
-import { Tree, prettyPrint } from '../bst/bst.js';
+import { Tree } from '../bst/bst.js';
 
 const svgElement = document.querySelector('#tree-svg');
 const addButton = document.querySelector('#add-button');
 const rebalanceButton = document.querySelector('#rebalance-button');
-const numberInput = document.querySelector('#number-input');
+const nodeRadius = 20;
+const initialSpacing = 3;
+const verticalSpacing = 130;
+const maxDepth = 6;
 
 let tree = new Tree([]);
 
 function renderTree(rootNode, svgElement) {
   svgElement.innerHTML = ''; // Clear the SVG content
 
-  const nodeRadius = 20;
-  const horizontalSpacing = 40;
-  const verticalSpacing = 60;
-
-  const drawNode = (node, x, y, level, dx) => {
+  const drawNode = (node, x, y, level = 0) => {
     if (!node) return;
 
-    const circle = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'circle'
-    );
-    circle.setAttribute('cx', x);
-    circle.setAttribute('cy', y);
-    circle.setAttribute('r', nodeRadius);
-    svgElement.appendChild(circle);
+    const spacing = Math.pow(2, maxDepth - level) * initialSpacing;
+    const circle = CreateCircle(x, y, node);
+    const text = CreateText(x, y, node);
 
-    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-    text.setAttribute('x', x);
-    text.setAttribute('y', y);
-    text.setAttribute('dy', '0.3em');
-    text.setAttribute('text-anchor', 'middle');
-    text.setAttribute('class', 'text');
-    text.textContent = node.value;
+    svgElement.appendChild(circle);
     svgElement.appendChild(text);
 
     if (node.left) {
-      const leftX = x - horizontalSpacing - verticalSpacing;
+      const leftX = x - spacing;
       const leftY = y + verticalSpacing;
       svgElement.appendChild(
         drawLine(x, y + nodeRadius, leftX, leftY - nodeRadius)
       );
-      drawNode(node.left, leftX, leftY);
+      drawNode(node.left, leftX, leftY, level + 1);
     }
 
     if (node.right) {
-      const rightX = x + horizontalSpacing + verticalSpacing;
+      const rightX = x + spacing;
       const rightY = y + verticalSpacing;
       svgElement.appendChild(
         drawLine(x, y + nodeRadius, rightX, rightY - nodeRadius)
       );
-      drawNode(node.right, rightX, rightY);
+      drawNode(node.right, rightX, rightY, level + 1);
     }
   };
 
@@ -61,38 +49,98 @@ function renderTree(rootNode, svgElement) {
     line.setAttribute('x2', x2);
     line.setAttribute('y2', y2);
     line.setAttribute('stroke', 'black');
+
     return line;
   };
 
   if (rootNode) {
-    drawNode(rootNode, svgElement.clientWidth / 2, nodeRadius);
+    drawNode(rootNode, svgElement.clientWidth / 2, nodeRadius, 0);
   }
 }
 
-function updateTree() {
-  const values = randomArray(5, 100);
-  tree.root = tree.buildTree([...new Set(values)]);
-  renderTree(tree.root, svgElement);
-}
+// Event Listeners
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addValue();
+});
+
+rebalanceButton.addEventListener('click', rebalanceTree);
 
 function addValue() {
   const value = document.querySelector('#add-value').value;
+  if (!value) return;
+  if (value < 0 || value > 1000)
+    return alert('Value must be between 0 and 1000');
+  if (isNaN(value)) return alert('Value must be a number');
   tree.insert(value);
+  renderTree(tree.root, svgElement);
+  document.querySelector('#add-value').value = '';
+}
+
+function removeValue(value) {
+  tree.delete(value);
   renderTree(tree.root, svgElement);
 }
 
 function rebalanceTree() {
-  tree = tree.rebalance();
-  prettyPrint(tree.root);
+  tree.rebalance();
   renderTree(tree.root, svgElement);
 }
 
-addButton.addEventListener('click', addValue);
-rebalanceButton.addEventListener('click', rebalanceTree);
+// Initialization
+function initTree() {
+  const values = RandomArray(3, 100);
+  tree.root = tree.buildTree([...new Set(values)]);
+  renderTree(tree.root, svgElement);
+}
+initTree();
 
-updateTree();
-
-// Random array generator
-function randomArray(length, max) {
+// Helper Functions
+function RandomArray(length, max) {
   return [...new Array(length)].map(() => Math.round(Math.random() * max));
+}
+
+function CreateCircle(x, y, node) {
+  const circle = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'circle'
+  );
+  circle.setAttribute('cx', x);
+  circle.setAttribute('cy', y);
+  circle.setAttribute('r', nodeRadius);
+
+  // add on click event listener to circle to remove node
+  circle.addEventListener('click', () => removeValue(node.value));
+
+  // make circle high light on hover
+  circle.addEventListener('mouseover', () => {
+    circle.setAttribute('stroke', 'red');
+    circle.setAttribute('stroke-width', '3');
+    circle.setAttribute('cursor', 'pointer');
+  });
+
+  // make circle normal on mouse out
+  circle.addEventListener('mouseout', () => {
+    circle.setAttribute('stroke', 'black');
+    circle.setAttribute('stroke-width', '1');
+  });
+
+  // make circle high z index
+  circle.setAttribute('style', 'z-index: 1000');
+
+  return circle;
+}
+
+function CreateText(x, y, node) {
+  const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+  text.setAttribute('x', x);
+  text.setAttribute('y', y);
+  text.setAttribute('dy', '0.3em');
+  text.setAttribute('text-anchor', 'middle');
+  text.setAttribute('class', 'text');
+  text.textContent = node.value;
+  // set text as non selectable or interactive
+  text.setAttribute('pointer-events', 'none');
+
+  return text;
 }
